1998bbf4788edaa1a16cb3c1a33c0a7c72eb4f63
Trevor Masinelli <trevor.m@thezenorange.com>
2017-01-24 21:47:53 -0600

    parse out html content to its own function

--- main.py
+++ main.py
@@ -17,55 +17,41 @@
 import webapp2
 import caesar
 
+def pageSetup(enc_content):
+    up_content = '''
+        <html>
+        <head><title>STLCC - LC101 - Web Caesar</title></head>
+        <body>
+        <h2>Web Caesar - STLCC LC101</h2>
+        <strong>Please enter some text for conversion:</strong><br />
+    '''
+    text_content = '<textarea name="content" style="height: 125px; width: 350px;">' + enc_content + '</textarea><br />'
+    code_content = '''
+        <br /><strong>Please enter your encryption code:</strong><br />
+        <input type="number" name="code" />
+        <br />
+    '''
+    button_content = '<br /><input type="submit" value="Submit" />'
+    down_content = '</body></html>'
+    form_start = '<form method="post">'
+    form_end = '</form>'
+    form = form_start + text_content + code_content + button_content + form_end
+    form_content = up_content + form + down_content
+
+    return form_content
+
+
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
-        up_content = '''
-            <html>
-            <head><title>STLCC - LC101 - Web Caesar</title></head>
-            <body>
-            <h2>Please enter some text for conversion:</h2>
-        '''
-        text_content_start = '<textarea name="content" style="height: 125px; width: 350px;">'
-        text_content_end = '</textarea><br />'
-        code_content = '''
-            <h5>Please enter your encryption code:</h5>
-            <input type="text" name="code" />
-            <br />
-        '''
-        button_content = '<input type="submit" value="Submit" />'
-        down_content = '</body></html>'
-        form_start = '<form method="post">'
-        form_end = '</form>'
-        form = form_start + text_content_start + text_content_end + code_content + button_content + form_end
-        raw_content = up_content + form + down_content
-
-        self.response.write(raw_content)
+        the_content = pageSetup("")
+        self.response.write(the_content)
 
 
     def post(self):
         enc_content = caesar.encrypt(self.request.get("content"), self.request.get("code"))
-        up_content = '''
-            <html>
-            <head><title>STLCC - LC101 - Web Caesar</title></head>
-            <body>
-            <h2>Please enter some text for conversion:</h2>
-        '''
-        text_content_start = '<textarea name="content" style="height: 125px; width: 350px;">'
-        text_content_end = '</textarea><br />'
-        code_content = '''
-            <h5>Please enter your encryption code:</h5>
-            <input type="text" name="code" />
-            <br />
-        '''
-        button_content = '<input type="submit" value="Submit" />'
-        down_content = '</body></html>'
-        form_start = '<form method="post">'
-        form_end = '</form>'
-        form = form_start + text_content_start + enc_content + text_content_end + code_content + button_content + form_end
-        encrypted_content = up_content + form + down_content
-
-        self.response.write(encrypted_content)
+        the_content = pageSetup(enc_content)
+        self.response.write(the_content)
 
 
 app = webapp2.WSGIApplication([
